61b1dbd4ba3e5152e1917727c745a3d2eac08529
Aabid <sayeedaabid0@gmail.com>
2024-01-25 10:44:01 +0530

    adds picasso jar file

--- android/app/src/main/kotlin/com/example/method_channel/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/method_channel/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.example.method_channel
+import android.util.Log
 import androidx.annotation.NonNull
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-
-
 class MainActivity: FlutterActivity() {
     private val channel = "a-b-s.com/native-code-example"
-
     override fun configureFlutterEngine(@NonNull flutterEngine:FlutterEngine){
         super.configureFlutterEngine(flutterEngine)
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger,channel).setMethodCallHandler{
@@ -17,15 +17,28 @@ class MainActivity: FlutterActivity() {
             if(call.method == "messageFromNativeCode"){
                 var message = getMessage()
                 if(message.isNotEmpty()){
-                    result.success(message)
+                    val imageUrl = "https://jpeg.org/images/jpeg-home.jpg"
+                    try{
+                      val myImage = Picasso.with(this)
+                                .load(imageUrl).fetch(object : Callback {
+                                  override fun onSuccess() {
+                                      Log.d("ImageSize","Downloaded image  success")
+                                      result.success("Image Downloaded successfully -> $message")
+                                  }
+                                  override fun onError() {
+                                      result.error("UNAVAILABLE","Imaged download failed",null)
+                                  }
+                              })
+                    } catch (e:Exception){
+                        Log.d("ImageSize","image size ${e.message} ")
+
+                    }
                 }else{
                     result.error("UNAVAILABLE","Message from kotlin code is empty",null)
                 }
             }else{
                 result.notImplemented()
             }
-
-
         }
     }
     private fun getMessage():String{
